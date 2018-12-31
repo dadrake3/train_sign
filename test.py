4f8f4b65e9ae43fe22d59db7f3649e917459660b
Daryl Drake <dadrake3@gmail.com>
2018-12-31 03:59:47 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-# DriverAdaMatrix provides a driver plugin for use with BiblioPixel: https://github.com/ManiacalLabs/BiblioPixel
-# BiblioPixel is a write once, run on anything interface for LED animations on just about any hardware
-# Install with "pip install bibliopixel" or download from GitHub
-# Due to the Adafruit_RGBmatrix requirement, this code will only run on the Raspberry Pi
+# # DriverAdaMatrix provides a driver plugin for use with BiblioPixel: https://github.com/ManiacalLabs/BiblioPixel
+# # BiblioPixel is a write once, run on anything interface for LED animations on just about any hardware
+# # Install with "pip install bibliopixel" or download from GitHub
+# # Due to the Adafruit_RGBmatrix requirement, this code will only run on the Raspberry Pi
 
 import time
 # from rgbmatrix import Adafruit_RGBmatrix
@@ -32,31 +32,43 @@ class DriverAdaMatrix(DriverBase):
     #     self._matrix.SetPWMBits(bits)
 
 
-# #Usage is as follows:
-# #See the Wiki for more details: https://github.com/ManiacalLabs/BiblioPixel/wiki
+# # #Usage is as follows:
+# # #See the Wiki for more details: https://github.com/ManiacalLabs/BiblioPixel/wiki
 
-from bibliopixel import *
-import bibliopixel.colors as colors
-from bibliopixel.animation import *
+# from bibliopixel import *
+# import bibliopixel.colors as colors
+# from bibliopixel.animation import *
 
-driver = DriverAdaMatrix(rows=16, chain=1)
-# driver.SetPWMBits(6) #decrease bit-depth for better performance
-#MUST use serpentine=False because rgbmatrix handles the data that way
+driver = DriverAdaMatrix(rows=16, chain=2)
+# # driver.SetPWMBits(6) #decrease bit-depth for better performance
+# #MUST use serpentine=False because rgbmatrix handles the data that way
 led = Matrix(driver, 16, 32, serpentine=False)
 
-#Must have code downloaded from GitHub for matrix_animations
-# from matrix_animations import *
-# import bibliopixel.log as log
-# log.setLogLevel(log.DEBUG)
+# #Must have code downloaded from GitHub for matrix_animations
+# # from matrix_animations import *
+# # import bibliopixel.log as log
+# # log.setLogLevel(log.DEBUG)
 
-#load matrix calibration test animation
+# #load matrix calibration test animation
+# from bibliopixel.animation import MatrixCalibrationTest
+# anim = MatrixCalibrationTest(led)
+
+# try:
+#     #run the animation
+#     anim.run()
+# except KeyboardInterrupt:
+#     #Ctrl+C will exit the animation and turn the LEDs offs
+#     led.all_off()
+#     led.update()
+
+from bibliopixel.layout import *
 from bibliopixel.animation import MatrixCalibrationTest
-anim = MatrixCalibrationTest(led)
+from bibliopixel.drivers.SPI.LPD8806 import *
+from bibliopixel.layout import Rotation
 
-try:
-    #run the animation
-    anim.run()
-except KeyboardInterrupt:
-    #Ctrl+C will exit the animation and turn the LEDs offs
-    led.all_off()
-    led.update()
+#create driver for a 12x12 grid, use the size of your display
+# driver = LPD8806(12*12)
+# led = Matrix(driver)
+
+anim = MatrixCalibrationTest(led)
+anim.run()
